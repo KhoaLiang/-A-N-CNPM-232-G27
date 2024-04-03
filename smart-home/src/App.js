3f0488b3ebd8c7d2bf460106e7001d526fc13a2c
KhoaLiang <akhoa2020@gmail.com>
2024-04-03 10:08:16 +0700

    header, login page, footer
    Placeholder header, footer
    Finished login page

--- smart-home/src/App.js
+++ smart-home/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 // Import your page components
 import Dashboard from './pages/Dashboard';
@@ -9,11 +11,69 @@ import ElectricityUsagePage from './pages/ElectricityUsagePage';
 import SettingsPage from './pages/SettingsPage';
 import LoginPage from './pages/LoginPage';
 
+const Header = () => {
+    const location = useLocation();
+    const isLoginPage = location.pathname === '/login';
+
+    //uncomment this when it's time to implement the login page properly
+    // if (isLoginPage) {
+    //     return null;
+    // } 
+
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" ><Link to="/">Dashboard</Link></a>
+                <a class="nav-link" ><Link to="/sensor">Sensor</Link></a>
+                <a class="nav-link" ><Link to="/electricity-usage">Electricity Usage</Link></a>
+                <a class="nav-link" ><Link to="/settings">Settings</Link></a>
+                <a class="nav-link" ><Link to="/login">Login/Logout</Link></a>
+              </div>
+            </div>
+          </div>
+        </nav>
+    );
+};
+
+const Footer = () => {
+    const location = useLocation();
+    const isLoginPage = location.pathname === '/login';
+
+    if (isLoginPage) {
+        return null;
+    }
+
+    return (
+        <nav class="navbar fixed-bottom navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+    );
+};
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
+        {/* <nav>
           <ul>
             <li>
               <Link to="/">Dashboard</Link>
@@ -31,8 +91,8 @@ function App() {
               <Link to="/login">Login/Logout</Link>
             </li>
           </ul>
-        </nav>
-
+        </nav> */}
+        <Header />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/sensor" element={<SensorPage />} />
@@ -40,6 +100,7 @@ function App() {
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
--- smart-home/src/pages/LoginPage.jsx
+++ smart-home/src/pages/LoginPage.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import '../css/LoginPage.css';
+import logo from '../img/Logo.png';
 const LoginPage = () => {
     return (
-        <div>
-            <h1>Welcome to our Smart Home App!</h1>
-            <p>Login here</p>
-        </div>
+        
+            <div className='container vh-100 align-items-center'>
+                <div className='row justify-content-center align-items-center d-flex vh-100'>
+                    <form className='col-4 border border-dark p-3 rounder-border login-form'>
+                        <img src={logo} alt="" className='logo'/>
+                        <div className='form-group m-3'>
+                            <input type='text' className='form-control' id='username' placeholder='Enter Username'/>
+                        </div>
+                        <div className='form-group m-3'>
+                            <input type='password' className='form-control' id='password' placeholder='Enter Password'/>
+                        </div>
+                        <button type='submit' className='btn btn-primary'>Login</button>
+                        <p className='text-center mt-3'><a href='#'>Forget password?</a></p>
+                    </form>
+                </div>
+            </div>
     );
 };
 
