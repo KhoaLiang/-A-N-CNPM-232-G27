7700112c3d9ad4c6f57ab40b5dc038a3a68ec4e5
Dang12312 <80524787+Dang12312@users.noreply.github.com>
2024-04-03 23:26:32 +0700

    fix database phase 2

---
+++ smart-home/src/pages/SettingsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingsPage = () => {
+    return (
+        <div>
+            <h1>Welcome to our Smart Home App!</h1>
+            <p>This is where you edit your setting</p>
+        </div>
+    );
+};
+
+export default SettingsPage;
