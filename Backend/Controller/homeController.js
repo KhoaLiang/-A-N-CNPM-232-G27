598f8401feb69c6d5857dc85d68210289a27e3bf
Dang12312 <80524787+Dang12312@users.noreply.github.com>
2024-04-12 23:57:29 +0700

    update controller

---
+++ Backend/Controller/homeController.js
@@ -0,0 +1,216 @@
+// import client, { topicRes, topicReq } from '../mqtt/index.js';
+import { Room, Device, Port, Stat } from '../models/index.js'
+import { genId, updateStat } from './Utils.js'
+// import ada from '../api/adafruit.js';
+// import axios from 'axios';
+class HomeController {
+  show = (req, res, next) => {
+    const { room } = req.query
+    Room.find({}, 'name id')
+      .then((rooms) => {
+        let currentRoomId
+        if (room) {
+          for (let i = 0; i < rooms.length; i++) {
+            if (rooms[i].name.toLowerCase().split(' ').join('-') === room) {
+              currentRoomId = rooms[i].id
+              break
+            }
+          }
+        } else {
+          currentRoomId = rooms[0].id
+        }
+
+        Device.find({ roomId: currentRoomId })
+          .then((devices) => {
+            // devices.map((device) => {
+            //     if (device.type ==='led') {
+            //         icon = 'lightbulb'
+            //     }
+            //     else if (device.typ === 'fan') {
+            //         icon = ''
+            //     }
+            // })
+            res.render('index', {
+              rooms: rooms,
+              currentRoomId: currentRoomId,
+              devices: devices,
+              temp: '--',
+              humi: '--',
+            })
+          })
+          .catch((err) => console.log(err))
+      })
+      .catch((err) => console.log(err))
+  }
+  addNewRoom = (req, res, next) => {
+    const roomName = req.body.name
+    const newRoom = new Room({
+      id: genId(),
+      name: roomName,
+    })
+    newRoom
+      .save()
+      .then((result) => {
+        res.status(200).json({
+          status: 200,
+          data: result,
+        })
+      })
+      .catch((err) => console.log(err))
+  }
+  getNewDevice = async (req, res, next) => {
+    const emptyPorts = await Port.find({ status: false })
+    if (emptyPorts.length === 0) {
+      res.status(200).json({
+        status: 404,
+        message: 'All ports have been used',
+      })
+    } else {
+      res.status(200).json({
+        status: 200,
+        data: emptyPorts,
+      })
+    }
+  }
+  addNewDevice = (req, res, next) => {
+    const { deviceName, deviceId, deviceType, roomId } = req.body
+    const newDevice = new Device({
+      id: deviceId,
+      name: deviceName,
+      status: false,
+      type: deviceType,
+      roomId: roomId,
+      capacity: deviceType === 'fan' ? 65 : 36,
+      lastUse: new Date(),
+    })
+    newDevice
+      .save()
+      .then((result) => {
+        Port.findOneAndUpdate({ port: deviceId }, { status: true })
+          .then((_) => {
+            const deviceStat = new Stat({
+              deviceId: deviceId,
+            })
+            deviceStat
+              .save()
+              .then((_) => {
+                res.status(200).json({
+                  status: 200,
+                  data: result,
+                })
+              })
+              .catch((err) => console.log(err))
+          })
+          .catch((err) => console.log(err))
+      })
+      .catch((err) => console.log(err))
+  }
+  toggleDevice = (req, res, next) => {
+    const { deviceId, deviceType, status } = req.body
+    // Turn off
+    if (status === 'false') {
+      Device.findOne({ id: deviceId })
+        .then((result) => {
+          const currentTime = new Date()
+          const usedTime = currentTime - result.lastUse
+          const lastDuration = result.duration
+          Device.findOneAndUpdate(
+            { id: deviceId },
+            {
+              status: false,
+              duration: usedTime + lastDuration,
+            }
+          )
+            .then(async (result2) => {
+              try {
+                const result3 = await updateStat(
+                  deviceId,
+                  usedTime,
+                  result.lastUse
+                )
+                if (result3) {
+                  res.status(200).json({
+                    status: 200,
+                    data: result2,
+                  })
+                }
+              } catch (error) {
+                res.status(200).json({
+                  status: 500,
+                  message: error,
+                })
+              }
+            })
+            .catch((err) => console.log(err))
+        })
+        .catch((err) => console.log(err))
+    } else if (status === 'true') {
+      Device.findOneAndUpdate(
+        { id: deviceId },
+        {
+          status: true,
+          lastUse: new Date(),
+        }
+      )
+        .then((result) => {
+          res.status(200).json({
+            status: 200,
+            data: result,
+          })
+        })
+        .catch((err) => console.log(err))
+    }
+  }
+  deleteDevice = (req, res, next) => {
+    const _id = req.body.id
+    Device.findOneAndDelete({ id: _id })
+      .then((_) => {
+        Port.findOneAndUpdate({ port: _id }, { status: false })
+          .then((_) => {
+            Stat.findOneAndDelete({ deviceId: _id })
+              .then((deletedResult) => {
+                res.status(200).json({
+                  status: 200,
+                })
+              })
+              .catch((err) => console.log(err))
+          })
+          .catch((err) => console.log(err))
+      })
+      .catch((err) => console.log(err))
+  }
+  updateDevice = (req, res, next) => {
+    const { deviceId, deviceName } = req.body
+    console.log(deviceId, deviceName)
+    Device.findOneAndUpdate({ id: deviceId }, { name: deviceName })
+      .then((result) => {
+        res.status(200).json({
+          status: 200,
+          data: result,
+          message: 'Update device successfully',
+        })
+      })
+      .catch((err) => console.log(err))
+  }
+  getAllDevice = (req, res, next) => {
+    Device.find()
+      .then((result) =>
+        res.status(200).json({
+          status: 200,
+          data: result,
+        })
+      )
+      .catch((err) => console.log(err))
+  }
+  getAllActiveDevice = (req, res, next) => {
+    Device.find({ status: true })
+      .then((result) => {
+        res.status(200).json({
+          status: 200,
+          data: result,
+        })
+      })
+      .catch((err) => console.log(err))
+  }
+}
+export default new HomeController()
---
+++ Backend/Controller/mqttController.js
@@ -0,0 +1,11 @@
+class AdaController {
+  getIOKey = (req, res, next) => {
+    // const key = process.env.IO_KEY
+    res.status(200).json({
+      status: 200,
+      key: key,
+    })
+  }
+}
+
+export default new AdaController()
