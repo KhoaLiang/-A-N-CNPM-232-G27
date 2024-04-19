04dea41de2096a241ab540d6d8b1185794dfe3f7
Dang12312 <80524787+Dang12312@users.noreply.github.com>
2024-04-19 18:47:59 +0700

    update IO key

--- Back-end/mqtt.js
+++ Back-end/mqtt.js
@@ -1,37 +1,37 @@
-const mqtt = require('mqtt')
-const dotenv = require('dotenv')
+// const mqtt = require('mqtt')
+// const dotenv = require('dotenv')
 
-dotenv.config({ path: './config.env' })
+// dotenv.config({ path: './config.env' })
 
-const AIO_USERNAME = process.env.AIO_USERNAME
-const AIO_KEY = process.env.AIO_KEY
-const AIO_FEED = process.env.AIO_FEED // Optional: Replace with your feed name
+// const AIO_USERNAME = process.env.AIO_USERNAME
+// const AIO_KEY = process.env.AIO_KEY
+// const AIO_FEED = process.env.AIO_FEED // Optional: Replace with your feed name
 
-// MQTT Client Setup
-const client = mqtt.connect('mqtt://io.adafruit.com', {
-  username: AIO_USERNAME,
-  password: AIO_KEY,
-})
+// // MQTT Client Setup
+// const client = mqtt.connect('mqtt://io.adafruit.com', {
+//   username: AIO_USERNAME,
+//   password: AIO_KEY,
+// })
 
-// Event: 'connect'
-client.on('connect', () => {
-  console.log('Connected to Adafruit IO MQTT Broker')
-  // Subscribe to a feed (optional)
-  client.subscribe(`${AIO_USERNAME}/feeds/${AIO_FEED}`)
-})
+// // Event: 'connect'
+// client.on('connect', () => {
+//   console.log('Connected to Adafruit IO MQTT Broker')
+//   // Subscribe to a feed (optional)
+//   client.subscribe(`${AIO_USERNAME}/feeds/${AIO_FEED}`)
+// })
 
-// Event: 'message'
-client.on('message', (topic, message) => {
-  console.log(
-    `Received message from topic: ${topic}, message: ${message.toString()}`
-  )
-  // Process the received message
-})
+// // Event: 'message'
+// client.on('message', (topic, message) => {
+//   console.log(
+//     `Received message from topic: ${topic}, message: ${message.toString()}`
+//   )
+//   // Process the received message
+// })
 
-// Publish a message (optional)
-client.publish(`${AIO_USERNAME}/feeds/${AIO_FEED}`, 'Hello from MQTT client!')
+// // Publish a message (optional)
+// client.publish(`${AIO_USERNAME}/feeds/${AIO_FEED}`, 'Hello from MQTT client!')
 
-// Handle errors
-client.on('error', (err) => {
-  console.error('Error occurred:', err)
-})
+// // Handle errors
+// client.on('error', (err) => {
+//   console.error('Error occurred:', err)
+// })
